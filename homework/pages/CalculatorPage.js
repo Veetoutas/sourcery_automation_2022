90e28eed1a02893589967d97b0200827a5cf0486
DESKTOP-4MS04CV\User <vyt.matulevicius@gmail.com>
2022-07-23 09:58:48 +0300

    Ok, this is the FINAL final commit.

--- homework/pages/CalculatorPage.js
+++ homework/pages/CalculatorPage.js
@@ -1,3 +1,4 @@
+// Main repetitive variables
 let firstField = '#number1Field';
 let secondField = '#number2Field';
 let calculateButton = '#calculateButton';
@@ -5,29 +6,35 @@ let testPage = 'https://testsheepnz.github.io/BasicCalculator';
 let buildSelector = '#selectBuild';
 let operationSelector = '#selectOperationDropdown';
 
+// Main Calculator functions
 class CalculatorPage {
 
     constructor(page) {
         this.page = page;
       }
 
+    // goes to Calculator website
     async navigate() {
         await this.page.goto(testPage);
     }
 
+    // Selects Calculator build version
     async versionSelect(version) {
         await this.page.selectOption(buildSelector, { label: version});
     }
 
+    // Fills number fields values
     async fillNumberFields(firstValue, secondValue) {
         await this.page.locator(firstField).type(firstValue);
         await this.page.locator(secondField).type(secondValue);
     }
     
+    // Selects operation type
     async selectOperation(operation) {
         await this.page.selectOption(operationSelector, {label: operation});  
     }
 
+    // Clicks the Calculate button
     async calculateResult() {
         await this.page.locator(calculateButton).click();
     }
